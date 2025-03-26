c930954583737fd8eee488f63398431302a25e83
Bhavy2003 <bhavy.j@ahduni.edu.in>
2025-03-26 15:43:34 +0530

    Ensuregg

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,3 +1,57 @@
+// import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// import authSlice from "./authSlice";
+// import jobSlice from "./jobSlice";
+// import {
+//     persistStore,
+//     persistReducer,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import companySlice from "./companySlice";
+// import applicationSlice from "./applicationSlice";
+
+// const persistConfig = {
+//     key: 'root',
+//     version: 1,
+//     storage,
+// };
+
+
+// const appReducer = combineReducers({
+//     auth: authSlice,
+//     job: jobSlice,
+//     company: companySlice,
+//     application: applicationSlice,
+// });
+
+// const rootReducer = (state, action) => {
+//     if (action.type === 'LOGOUT') {
+       
+//         storage.removeItem('persist:root');
+//         state = undefined;
+//     }
+//     return appReducer(state, action);
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = configureStore({
+//     reducer: persistedReducer,
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware({
+//             serializableCheck: {
+//                 ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//             },
+//         }),
+// });
+
+// export default store;
+
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import jobSlice from "./jobSlice";
@@ -11,14 +65,14 @@ import {
     PURGE,
     REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session'; // Use session storage
 import companySlice from "./companySlice";
 import applicationSlice from "./applicationSlice";
 
 const persistConfig = {
     key: 'root',
     version: 1,
-    storage,
+    storage: storageSession,  // 🔥 Use sessionStorage instead of localStorage
 };
 
 // Root reducer with reset logic
@@ -31,8 +85,8 @@ const appReducer = combineReducers({
 
 const rootReducer = (state, action) => {
     if (action.type === 'LOGOUT') {
-        // Clear the persisted state
-        storage.removeItem('persist:root');
+        // Clear the persisted state on logout
+        sessionStorage.removeItem('persist:root');
         state = undefined;
     }
     return appReducer(state, action);
@@ -51,3 +105,4 @@ const store = configureStore({
 });
 
 export default store;
+
