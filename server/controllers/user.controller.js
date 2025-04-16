62b1263ef9ffc314d51b854b657acfcf67e9dd10
Bhavy2003 <bhavyjhaveri@gmail.com>
2025-04-16 17:03:43 +0530

    profilesignup

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,12 +6,12 @@ import cloudinary from "../utils/cloudinary.js";
 
 export const register = async(req, res) => {
     try {
-        const { fullname, email, password, role, phoneNumber,confirmPassword} = req.body;
+        const { fullname, email, password, role, phoneNumber} = req.body;
 
         
 
         // Check if any required field is missing
-        if (!fullname || !email || !password || !role || !phoneNumber || !confirmPassword) {
+        if (!fullname || !email || !password || !role || !phoneNumber ) {
             return res.status(400).json({
                 message: "Required All Fileds",
                 success: false,
@@ -43,7 +43,7 @@ export const register = async(req, res) => {
 
             password: hashedPassword,
             role,
-            confirmPassword,
+           
             phoneNumber,
             profile: {
                 profilePhoto: profilePhotoUrl,
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -18,10 +18,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    confirmPassword: {
-        type: String,
-        required: true,
-    },
     role: {
         type: String,
         enum: ['student', 'recruiter'],
